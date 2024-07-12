48e178ac17887e268129406d83d56a6f0f8982d6
Zeinabeftekhari <z.eftekhari@uq.edu.au>
2024-07-12 18:49:44 +1000

    adding functions

--- libraries/FID-A/inputOutput/io_loadspec_mat.m
+++ libraries/FID-A/inputOutput/io_loadspec_mat.m
@@ -2,65 +2,43 @@ function out=io_loadspec_mat(filename);
 csi = load(filename, 'csi').csi;
 ReadInInfo = load(filename , 'ReadInInfo').ReadInInfo;
 Par = load(filename , 'Par').Par;
-fids = csi(30,30,15,:); %mask information later on will add here,
-reshape_size = size(fids);
-new_size = [reshape_size(4) 1 1];
-fids = reshape(fids, new_size); 
+
+fids = load_reshape_fids(csi);
+
 dims.t = 1;
-specs = fftshift(fft(fids,[],dims.t),dims.t);
-sz=size(fids);
-%calculate ppm, t ,spectralwidth, dwelltime, txfrq, B0
-txfrq = ReadInInfo.Par.LarmorFreq/1e6;
-dwelltime = ReadInInfo.Par.Dwelltime;
-B0 = txfrq/42.577;
-spectralwidth = 1e9 / dwelltime;
-frequency_step = spectralwidth / sz(1);
-frequency_start = -spectralwidth / 2 + frequency_step / 2;
-frequency_end = spectralwidth / 2 - frequency_step / 2;
-f = frequency_start:frequency_step:frequency_end;
-ppm = f / txfrq;
-% Siemens data assumes the center frequency to be 4.7 ppm: % 4.65 from Vienna script
-centerFreq = 4.65;
-ppm=ppm + centerFreq;
-t=[0:dwelltime:(sz(1)-1)*dwelltime];
-date = '';
 dims.coils = 0; %this can be 0.
 dims.averages = 0; %this can be 0 if crash.
 dims.subSpecs = 0;
 dims.extras = 0;
-averages = ReadInInfo.Par.nAve; %number of voxel in the region 
-rawAverages = ReadInInfo.Par.nAve; %number of voxel in the region 
+
+specs = fftshift(fft(fids,[],dims.t),dims.t);
+sz=size(fids);
+B0 = txfrq/42.577;
+ppm = load_ppm(ReadInInfo);
+t=[0:dwelltime:(sz(1)-1)*dwelltime];
+averages = ReadInInfo.Par.nAve; %number of voxel in the region
+rawAverages = ReadInInfo.Par.nAve; %number of voxel in the region
 subspecs = 1;
-rawSubspecs = 1; 
-seq = 'MRSI_Vienna'; 
-if abs(7-B0) < 0.5  
-   TE = 1.3;
+rawSubspecs = 1;
+
+seq = 'MRSI_Vienna';
+
+date = '';
+
+if abs(7-B0) < 0.5
+    TE = 1.3;
 else
-    TE = 0.8; 
+    TE = 0.8;
 end
-if abs(7-B0) < 0.5  
-   TR = 460;
+if abs(7-B0) < 0.5
+    TR = 460;
 else
-    TR = 950; 
+    TR = 950;
 end
+
 leftshift = 0; %i am not sure, find it later.
-%these are stored in raw.geometry, I donot know how to store them in raw.geometry
-geometry.size.VoI_RoFOV     = ReadInInfo.Par.ReadVOI; % Voxel size in readout direction [mm]
-geometry.size.VoI_PeFOV     = ReadInInfo.Par.PhaseVOI; % Voxel size in phase encoding direction [mm]
-geometry.size.VoIThickness  = ReadInInfo.Par.SliceVOI; % Voxel size in slice selection direction [mm]
-geometry.pos.PosCor         = Par.CSI.PosVOI_Cor; % Coronal coordinate of voxel [mm]
-geometry.pos.PosSag         = Par.CSI.PosVOI_Sag; % Sagittal coordinate of voxel [mm]
-geometry.pos.PosTra         = Par.CSI.PosVOI_Tra; % Transversal coordinate of voxel [mm]
-geometry.pos.TablePosSag    = 0; % Sagittal table position [mm]. donot know
-geometry.pos.TablePosCor    = 0; % Coronal table position [mm]. donot know
-geometry.pos.TablePosTra    = 0; % Transversal table position [mm]. donot know
-geometry.rot.VoI_InPlaneRot = ReadInInfo.Par.InPlaneRotation; % Voxel rotation in plane
-geometry.rot.NormCor        = ReadInInfo.Par.SliceNormalVector_x; % Coronal component of normal vector of voxel. donot know
-geometry.rot.NormSag        = ReadInInfo.Par.SliceNormalVector_y; % Sagittal component of normal vector of voxel. donot know
-geometry.rot.NormTra        = ReadInInfo.Par.SliceNormalVector_z; % Transversal component of normal vector of voxel. donot know
-% nucleus = '1H';
-% software = 'vd syngo MR E12'; %not know about 3T
-% Manufacturer = 'Siemens'; 
+
+geometry = load_geometry(ReadInInfo,Par);
 
 %FILLING IN DATA STRUCTURE
 out.fids=fids;
@@ -103,4 +81,43 @@ if out.dims.subSpecs==0
     out.flags.isFourSteps=0;
 else
     out.flags.isFourSteps=(out.sz(out.dims.subSpecs)==4);
+end
+end
+
+function ppm = load_ppm(ReadInInfo)
+txfrq = ReadInInfo.Par.LarmorFreq/1e6;
+dwelltime = ReadInInfo.Par.Dwelltime;
+B0 = txfrq/42.577;
+spectralwidth = 1e9 / dwelltime;
+frequency_step = spectralwidth / sz(1);
+frequency_start = -spectralwidth / 2 + frequency_step / 2;
+frequency_end = spectralwidth / 2 - frequency_step / 2;
+f = frequency_start:frequency_step:frequency_end;
+ppm = f / txfrq;
+% Siemens data assumes the center frequency to be 4.7 ppm: % 4.65 from Vienna script
+centerFreq = 4.65;
+ppm=ppm + centerFreq;
+end
+
+function geometry = load_geometry(ReadInInfo,Par)
+geometry.size.VoI_RoFOV     = ReadInInfo.Par.ReadVOI; % Voxel size in readout direction [mm]
+geometry.size.VoI_PeFOV     = ReadInInfo.Par.PhaseVOI; % Voxel size in phase encoding direction [mm]
+geometry.size.VoIThickness  = ReadInInfo.Par.SliceVOI; % Voxel size in slice selection direction [mm]
+geometry.pos.PosCor         = Par.CSI.PosVOI_Cor; % Coronal coordinate of voxel [mm]
+geometry.pos.PosSag         = Par.CSI.PosVOI_Sag; % Sagittal coordinate of voxel [mm]
+geometry.pos.PosTra         = Par.CSI.PosVOI_Tra; % Transversal coordinate of voxel [mm]
+geometry.pos.TablePosSag    = 0; % Sagittal table position [mm].
+geometry.pos.TablePosCor    = 0; % Coronal table position [mm].
+geometry.pos.TablePosTra    = 0; % Transversal table position [mm].
+geometry.rot.VoI_InPlaneRot = ReadInInfo.Par.InPlaneRotation; % Voxel rotation in plane
+geometry.rot.NormCor        = ReadInInfo.Par.SliceNormalVector_x; % Coronal component of normal vector of voxel.
+geometry.rot.NormSag        = ReadInInfo.Par.SliceNormalVector_y; % Sagittal component of normal vector of voxel.
+geometry.rot.NormTra        = ReadInInfo.Par.SliceNormalVector_z; % Transversal component of normal vector of voxel.
+end
+
+function fids = load_reshape_fids(csi)
+fids = csi(30,30,15,:); %mask information later on will add here,
+reshape_size = size(fids);
+new_size = [reshape_size(4) 1 1];
+fids = reshape(fids, new_size);
 end
