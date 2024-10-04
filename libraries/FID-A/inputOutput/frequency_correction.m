925b10628576de1c6338ddebf3506a0d1742e34c
korbinian90 <korbinian90@gmail.com>
2024-10-04 19:49:21 +0200

    more refactoring

--- libraries/FID-A/inputOutput/frequency_correction.m
+++ libraries/FID-A/inputOutput/frequency_correction.m
@@ -1,12 +1,12 @@
-function fids = frequency_correction(fids, B0_map, dims, mask, spectralwidth, txfrq) %Frequency correction for each voxel before averaging
-    ppm_conversion = spectralwidth / size(fids, 1) / txfrq;
+function fids = frequency_correction(fids, B0_map, mask, params) %Frequency correction for each voxel before averaging
+    ppm_conversion = params.spectralwidth / size(fids, 1) / params.txfrq;
     B0_map(isnan(B0_map)) = 0; % Remove NaN's
     B0_map = reshape(B0_map(mask), 1, size(fids, 2)); %match B0 map to our mask in terms of size and location, we donot want B0 for whole brain.
     B0_map = -B0_map / ppm_conversion;
     B0_map = round(B0_map);
-    specs = fftshift(fft(fids, [], dims.t), dims.t);
+    specs = fftshift(fft(fids, [], 1), 1);
     specs = circshift(specs, B0_map);
-    fids = ifft(fftshift(specs, dims.t), [], dims.t);
+    fids = ifft(fftshift(specs, 1), [], 1);
     %t = t/10^9;
     %t = transpose(t);
     %B0CorrMat_Spec = exp(2*pi*1i*B0_map) .* t;
--- libraries/FID-A/inputOutput/io_loadspec_mat.m
+++ libraries/FID-A/inputOutput/io_loadspec_mat.m
@@ -4,83 +4,37 @@ function out = io_loadspec_mat(filename, SVS_mask_filename, B0_map_filename)
     % mask = load_mask_osprey(filename); %reading SVS mask for Osprey
     mask = logical(load_nifti(SVS_mask_filename));
     B0_map = load_nifti(B0_map_filename);
-    [fids, n_selected_voxels] = load_reshape_fids(csi, mask);
+    fids = load_reshape_fids(csi, mask);
 
     %% Load and set parameters
-    [txfrq, B0, dwelltime, spectralwidth, centerFreq, TE, TR] = load_parameters(ReadInInfo);
-    geometry = load_geometry(ReadInInfo, Par);
-    dims = set_dims();
-    sz = size(fids);
-    n_fid_points = sz(1);
-    t = 0:dwelltime:((n_fid_points - 1) * dwelltime); % time points for fids
-    ppm = calculate_ppm(txfrq, n_fid_points, spectralwidth, centerFreq); % ppm points for specs
-    averages = n_selected_voxels; %number of voxel in the region
-    rawAverages = n_selected_voxels; %number of voxel in the region
-    subspecs = 1;
-    rawSubspecs = 1;
-    seq = 'MRSI_Vienna';
-    date = '';
-    leftshift = 0;
+    params = load_parameters(ReadInInfo, Par, fids);
 
     %% Corrections
     %[fids] = linear_baseline_fitting(fids,dims);
-    fids = frequency_correction(fids, B0_map, dims, mask, spectralwidth, txfrq);
+    fids = frequency_correction(fids, B0_map, mask, params);
 
     %% Calculate spectrum from fids
-    specs = fftshift(fft(fids, [], dims.t), dims.t);
+    specs = fftshift(fft(fids, [], params.dims.t), params.dims.t);
 
-    %% Filling in data structure for Osprey
+    %% Filling in output structure
+    out = params;
     out.fids = fids;
     out.specs = specs;
-    out.sz = sz;
-    out.ppm = ppm;
-    out.t = t;
-    out.spectralwidth = spectralwidth;
-    out.dwelltime = dwelltime;
-    out.txfrq = txfrq;
-    out.date = date;
-    out.dims = dims;
-    out.Bo = B0;
-    out.averages = averages;
-    out.rawAverages = rawAverages;
-    out.subspecs = subspecs;
-    out.rawSubspecs = rawSubspecs;
-    out.seq = seq;
-    out.te = TE;
-    out.tr = TR;
-    out.pointsToLeftshift = leftshift;
-    out.centerFreq = centerFreq;
-    out.geometry = geometry;
+    out.flags = get_osprey_flags();
+end
 
-    % Sequence flags
-    out.flags.isUnEdited = 1;
-    out.flags.isMEGA = 0;
-    out.flags.isHERMES = 0;
-    out.flags.isHERCULES = 0;
-    out.flags.isPRIAM = 0;
-    out.flags.isMRSI = 0;
-
-    %FILLING IN THE FLAGS
-    out.flags.writtentostruct = 1;
-    out.flags.gotparams = 1;
-    out.flags.leftshifted = 0;
-    out.flags.filtered = 0;
-    out.flags.zeropadded = 0;
-    out.flags.freqcorrected = 0;
-    out.flags.phasecorrected = 0;
-    out.flags.averaged = 0;
-    out.flags.addedrcvrs = 1;
-    out.flags.subtracted = 0;
-    out.flags.writtentotext = 0;
-    out.flags.downsampled = 0;
-    out.flags.coilsCombined = 1;
-
-    if out.dims.subSpecs == 0
-        out.flags.isFourSteps = 0;
-    else
-        out.flags.isFourSteps = (out.sz(out.dims.subSpecs) == 4);
-    end
-
+function fids = load_reshape_fids(csi, mask)
+    %mask(:,:,:) = 0;
+    %mask(33:34,25:26,12:13) = 1;
+    n_selected_voxels = sum(mask, 'all');
+    n_time_points = size(csi, 4);
+    mask_4D = repmat(mask, 1, 1, 1, n_time_points);
+    selected_voxels = csi(mask_4D);
+    reshaped_voxels = reshape(selected_voxels, n_selected_voxels, n_time_points);
+    fids = transpose(reshaped_voxels);
+    fids = double(fids);
+    fids = fids ./ fids(3, :) .* abs(fids(3, :)); %this is the phase correction, using third point in fids.
+    %fids = flip(fids,1); %it might need to be flip for Osprey.
 end
 
 function [csi, ReadInInfo, Par] = load_mat(filename)
@@ -89,22 +43,16 @@ function [csi, ReadInInfo, Par] = load_mat(filename)
     Par = load(filename, 'Par').Par;
 end
 
-function [txfrq, B0, dwelltime, spectralwidth, centerFreq, TE, TR] = load_parameters(ReadInInfo)
-    txfrq = ReadInInfo.Par.LarmorFreq / 1e6;
-    B0 = txfrq / 42.577;
+function img = load_nifti(filename)
+    B0_map = nii_tool('load', filename);
+    img = B0_map.img;
+end
 
-    if abs(7 - B0) < 0.5
-        dwelltime = 2 * ReadInInfo.Par.Dwelltime;
-        TE = 1.3;
-        TR = 460;
-    else
-        dwelltime = ReadInInfo.Par.Dwelltime;
-        TE = 0.8;
-        TR = 950;
-    end
-
-    centerFreq = 4.65; % Siemens data assumes the center frequency to be 4.7 ppm: % 4.65 from Vienna script
-    spectralwidth = (1e9 / dwelltime);
+function mask = load_mask_osprey(CSI_filename)
+    dirname = fileparts(CSI_filename);
+    new_mask_name = 'SVS_mask.nii.gz';
+    mask_filename = fullfile(dirname, new_mask_name);
+    mask = load_nifti(mask_filename);
 end
 
 function ppm = calculate_ppm(txfrq, n_fid_points, spectralwidth, centerFreq)
@@ -113,7 +61,38 @@ function ppm = calculate_ppm(txfrq, n_fid_points, spectralwidth, centerFreq)
     frequency_end = spectralwidth / 2 - frequency_step / 2;
     f = frequency_start:frequency_step:frequency_end;
     ppm = f / txfrq;
-    ppm = ppm + centerFreq;
+    ppm = ppm + centerFreq;    
+    ppm = flip(ppm);
+end
+
+function params = load_parameters(ReadInInfo, Par, fids)
+    n_fid_points = size(fids, 1);
+    n_selected_voxels = size(fids, 2);
+    params.txfrq = ReadInInfo.Par.LarmorFreq / 1e6;
+    params.Bo = params.txfrq / 42.577;
+    if abs(7 - params.Bo) < 0.5
+        params.dwelltime = 2 * ReadInInfo.Par.Dwelltime;
+        params.TE = 1.3;
+        params.TR = 460;
+    else
+        params.dwelltime = ReadInInfo.Par.Dwelltime;
+        params.TE = 0.8;
+        params.TR = 950;
+    end
+    params.centerFreq = 4.65; % Siemens data assumes the center frequency to be 4.7 ppm: % 4.65 from Vienna script
+    params.spectralwidth = (1e9 / params.dwelltime);
+    params.geometry = load_geometry(ReadInInfo, Par);
+    params.dims = set_dims();
+    params.sz = size(fids);
+    params.t = 0:params.dwelltime:((n_fid_points - 1) * params.dwelltime); % time points for fids
+    params.ppm = calculate_ppm(params.txfrq, n_fid_points, params.spectralwidth, params.centerFreq); % ppm points for specs
+    params.averages = n_selected_voxels; %number of voxel in the region
+    params.rawAverages = n_selected_voxels; %number of voxel in the region
+    params.subspecs = 1;
+    params.rawSubspecs = 1;
+    params.seq = 'MRSI_Vienna';
+    params.date = '';
+    params.pointsToLeftshift = 0;
 end
 
 function geometry = load_geometry(ReadInInfo, Par)
@@ -132,18 +111,30 @@ function geometry = load_geometry(ReadInInfo, Par)
     geometry.rot.NormTra = ReadInInfo.Par.SliceNormalVector_z; % Transversal component of normal vector of voxel.
 end
 
-function [fids, n_selected_voxels] = load_reshape_fids(csi, mask)
-    %mask(:,:,:) = 0;
-    %mask(33:34,25:26,12:13) = 1;
-    n_selected_voxels = sum(mask, 'all');
-    n_time_points = size(csi, 4);
-    mask_4D = repmat(mask, 1, 1, 1, n_time_points);
-    selected_voxels = csi(mask_4D);
-    reshaped_voxels = reshape(selected_voxels, n_selected_voxels, n_time_points);
-    fids = transpose(reshaped_voxels);
-    fids = double(fids);
-    fids = fids ./ fids(3, :) .* abs(fids(3, :)); %this is the phase correction, using third point in fids.
-    %fids = flip(fids,1); %it might need to be flip for Osprey.
+function flags = get_osprey_flags()
+    % Sequence flags
+    flags.isUnEdited = 1;
+    flags.isMEGA = 0;
+    flags.isHERMES = 0;
+    flags.isHERCULES = 0;
+    flags.isPRIAM = 0;
+    flags.isMRSI = 0;
+
+    %FILLING IN THE FLAGS
+    flags.writtentostruct = 1;
+    flags.gotparams = 1;
+    flags.leftshifted = 0;
+    flags.filtered = 0;
+    flags.zeropadded = 0;
+    flags.freqcorrected = 0;
+    flags.phasecorrected = 0;
+    flags.averaged = 0;
+    flags.addedrcvrs = 1;
+    flags.subtracted = 0;
+    flags.writtentotext = 0;
+    flags.downsampled = 0;
+    flags.coilsCombined = 1;
+    flags.isFourSteps = 0;
 end
 
 function dims = set_dims()
@@ -152,17 +143,4 @@ function dims = set_dims()
     dims.averages = 2; %this can be 0 if crash.
     dims.subSpecs = 0;
     dims.extras = 0;
-end
-
-function mask = load_mask_osprey(CSI_filename)
-    dirname = fileparts(CSI_filename);
-    new_mask_name = 'SVS_mask.nii.gz';
-    mask_filename = fullfile(dirname, new_mask_name);
-    nii = nii_tool('load', mask_filename);
-    mask = nii.img;
-end
-
-function img = load_nifti(filename)
-    B0_map = nii_tool('load', filename);
-    img = B0_map.img;
 end
